60ff211cebdf62b3bd671d3c96461d5b85140a6d
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-11 23:25:20 +0700

    failed ui test

--- app/src/androidTest/java/com/example/empty/emptyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/empty/emptyapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.empty.emptyapp", appContext.packageName)
+        assertEquals("com.example.empty.wrong", appContext.packageName)
     }
 }
