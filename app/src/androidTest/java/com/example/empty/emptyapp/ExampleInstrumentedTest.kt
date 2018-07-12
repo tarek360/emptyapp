162a6f8addf9d6587abae962c881a052e58f890c
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-12 23:20:54 +0700

    make a trap

--- app/src/androidTest/java/com/example/empty/emptyapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/empty/emptyapp/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.example.empty.emptyapp
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +13,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.empty.wrong", appContext.packageName)
-    }
+
+  var master = ""
+
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getTargetContext()
+
+    val packageName = master + branch + "com.example.empty.emptyapp"
+
+    assertEquals(packageName, appContext.packageName)
+  }
+
+  var branch = ""
 }
