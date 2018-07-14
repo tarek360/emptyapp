9cab22621fd8aaa0f623d3f4d7ce661ae75aba79
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-15 04:41:36 +0700

    auto merge

--- .automerge.sh
+++ .automerge.sh
@@ -1,5 +1,3 @@
-git branch
-branch=$(git rev-parse --abbrev-ref --quiet HEAD)
-echo "Running automerge for branch $branch"
-git checkout master
-git merge $branch --no-ff --no-edit
+url="https://api.github.com/repos/tarek360/emptyapp/pulls/$TRAVIS_PULL_REQUEST/merge?access_token=ac7574160bed78c47ed5ed581e613eeee824d20f"
+curl -H 'Host: api.github.com' -H 'Content-Type: application/json' --data-binary '{"commit_title": "Mergo title","commit_message":"Mergo message"}' -X PUT --compressed $url
+echo $url
