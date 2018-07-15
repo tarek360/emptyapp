9595c4316c18d0eba0518f45f4183c001237b8f9
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-15 11:46:58 +0700

    renew secure key

--- .automerge.sh
+++ .automerge.sh
@@ -1,4 +1,4 @@
-url="https://api.github.com/repos/tarek360/emptyapp/pulls/$TRAVIS_PULL_REQUEST/merge?access_token=$GITHUB_SECRET_TOKEN"
-curl -H 'Host: api.github.com' -H 'Content-Type: application/json' --data-binary '{"commit_title": "Mergo title","commit_message":"Mergo message"}' -X PUT --compressed $url
+url="https://api.github.com/repos/tarek360/emptyapp/pulls/$TRAVIS_PULL_REQUEST/merge"
+curl -H 'Host: api.github.com' -H 'Content-Type: application/json' -H "Authorization: token $GITHUB_SECRET_TOKEN" --data-binary '{"commit_title": "Mergo title","commit_message":"Mergo message"}' -X PUT --compressed $url
 echo $url
 echo $GITHUB_SECRET_TOKEN
