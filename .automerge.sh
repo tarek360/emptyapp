8cd34b7376e1d254266c9249d34de66163ea51eb
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-15 11:17:13 +0700

    secure key

--- .automerge.sh
+++ .automerge.sh
@@ -1,3 +1,4 @@
-url="https://api.github.com/repos/tarek360/emptyapp/pulls/$TRAVIS_PULL_REQUEST/merge?access_token=ac7574160bed78c47ed5ed581e613eeee824d20f"
+url="https://api.github.com/repos/tarek360/emptyapp/pulls/$TRAVIS_PULL_REQUEST/merge?access_token=$GITHUB_SECRET_TOKEN"
 curl -H 'Host: api.github.com' -H 'Content-Type: application/json' --data-binary '{"commit_title": "Mergo title","commit_message":"Mergo message"}' -X PUT --compressed $url
 echo $url
+echo $GITHUB_SECRET_TOKEN
