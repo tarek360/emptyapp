03a58a20678612dff70e5eb25e234487a0d71861
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-22 00:13:42 +0700

    add env

--- .detekt-status.sh
+++ .detekt-status.sh
@@ -2,7 +2,7 @@ SHA=$(git rev-parse HEAD)
 PRE_LAST_SHA=$(git rev-parse @~)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-GITHUB_TOKEN=$GITHUB_SECRET_TOKEN
+TOKEN=$GITHUB_TOKEN
 PULL_REQUEST_ID=$TRAVIS_PULL_REQUEST
 OWNER_NAME="tarek360"
 REPO_NAME="emptyapp"
@@ -28,5 +28,5 @@ url="https://api.github.com/repos/$OWNER_NAME/$REPO_NAME/statuses/$SHA"
 echo "Create status.. for SHA: $SHA in PR: $PULL_REQUEST_ID"
 
 # Create status
-curl -H 'Host: api.github.com' -H 'Content-Type: application/json' -H "Authorization: token $GITHUB_TOKEN" --data-binary "{\"state\":\"${STATE}\", \"target_url\":\"${TARGET_URL}\", \"description\":\"${DESCRIPTION}\", \"context\":\"${CONTEXT}\"}" --compressed $url
+curl -H 'Host: api.github.com' -H 'Content-Type: application/json' -H "Authorization: token $TOKEN" --data-binary "{\"state\":\"${STATE}\", \"target_url\":\"${TARGET_URL}\", \"description\":\"${DESCRIPTION}\", \"context\":\"${CONTEXT}\"}" --compressed $url
 
