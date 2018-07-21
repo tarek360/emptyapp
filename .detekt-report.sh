735915cb201ee9160760f14f1aa12eeffb57af1b
Ahmed Tarek <a.tarek360@gmail.com>
2018-07-22 00:25:52 +0700

    remove env

--- .detekt-report.sh
+++ .detekt-report.sh
@@ -2,7 +2,7 @@ SHA=$(git rev-parse HEAD)
 PRE_LAST_SHA=$(git rev-parse @~)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-GITHUB_TOKEN=$GITHUB_SECRET_TOKEN
+TOKEN=$GITHUB_TOKEN
 PULL_REQUEST_ID=$TRAVIS_PULL_REQUEST
 OWNER_NAME="tarek360"
 REPO_NAME="emptyapp"
@@ -18,4 +18,4 @@ url="https://api.github.com/repos/$OWNER_NAME/$REPO_NAME/commits/$SHA/comments"
 
 # Create comment
 echo "Create comment.. for SHA: $SHA in PR: $PULL_REQUEST_ID"
-curl -H 'Host: api.github.com' -H 'Content-Type: application/json' -H "Authorization: token $GITHUB_TOKEN" --data-binary "{\"body\":\"${BODY}\"}" --compressed $url
+curl -H 'Host: api.github.com' -H 'Content-Type: application/json' -H "Authorization: token $TOKEN" --data-binary "{\"body\":\"${BODY}\"}" --compressed $url
